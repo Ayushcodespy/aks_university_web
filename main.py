50736a5c6a7fa101b5e26aacc4ae1353f1efb01c
Ayush <ayush.jnv54@gmail.com>
2024-08-16 16:49:24 +0530

    I have created a web page which shows the data from mySql database

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="ayush7085",
+    database="ayush"
+)
+
+cursor = mydb.cursor()
+cursor.execute("SELECT * FROM BATCH2022")
+
+result = cursor.fetchall()
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html", results=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
