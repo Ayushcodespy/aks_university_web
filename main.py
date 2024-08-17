d94c6eb23925c3ab2d1bc0d076dc9d507a9f84dd
Ayush <ayush.jnv54@gmail.com>
2024-08-18 05:23:27 +0530

    Accessing details of students from database by user authentication

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,18 +12,26 @@ mydb = mysql.connector.connect(
 )
 
 cursor = mydb.cursor()
-cursor.execute("SELECT * FROM BATCH2022")
-
-result = cursor.fetchall()
 
 @app.route('/')
 def home():
     return render_template("login.html")
 
 
-@app.route("/data")
-def hello_world():
-    return render_template("index.html", results=result)
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        username = request.form['user_id']
+        password = request.form['passwd']
+        login = cursor.execute(f"Select Student_ID, Password from login where Student_ID = '{username}' AND Password = '{password}'")
+        login = cursor.fetchone()
+        if (username == login[0] and password == login[1]):
+            students = cursor.execute(f"Select Students.* from Students, Login where Students.Student_ID = '{username}' and Students.Student_ID = Login.Student_ID")
+            students = cursor.fetchall()
+            return render_template('index.html', student=students)
+        else:
+            return render_template("login.html", error='Incorrect login details!')
+        
 
 
 if __name__ == "__main__":
