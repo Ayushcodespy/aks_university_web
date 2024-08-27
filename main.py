b8ba5b1b9a5afb4cef8b631e3635c613399e307c
Ayush <ayush.jnv54@gmail.com>
2024-08-27 23:13:22 +0530

    forgat password added by mail otp validation

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, Response, abort, g, redirect, render_template, request, session, url_for
+from flask_mail import Mail, Message
+import mysql.connector
+import random
+
 
 app = Flask(__name__)
 app.secret_key = "super-secret-key"
 
-import mysql.connector
+# mail = Mail(app)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'patelayush.satna@gmail.com'
+app.config['MAIL_PASSWORD'] = 'jlyw stso gslm vbey'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+# ------------------------------------------------------------------
+
 
 mydb = mysql.connector.connect(
     host="localhost",
@@ -36,7 +49,7 @@ def login():
         login = cursor.fetchone()
         if login :
             session['user'] = login[0]
-            return render_template('index.html')
+            return redirect(url_for('home'))
         else:
             return render_template("login.html", error='Incorrect login details!')
     return render_template('login.html')
@@ -51,12 +64,73 @@ def profile():
     
     else:
         return redirect(url_for('login'))
+    
+@app.route('/image/<student_id>')
+def get_image(student_id):
+    cursor.execute("SELECT Image FROM STUDENTS WHERE STUDENT_ID = %s", (student_id,))
+    image_data = cursor.fetchone()[0]
+
+    if image_data:
+        return Response(image_data, mimetype='image/jpeg')
+    else:
+        abort(404)
 
 @app.route("/logout")
 def logout():
     if 'user' in session:
         session.pop('user')
     return redirect(url_for('login'))
+
+
+@app.route("/forget_pwd")
+def forget_pwd():
+    return render_template("forget_pwd.html")
+
+@app.route("/send_otp", methods=['GET', 'POST'])
+def send_otp():
+    if request.method == 'POST':
+        student_code =request.form['student_code']
+        cursor.execute("Select email from login where student_id = %s",(student_code,))
+        email = cursor.fetchone()
+        email = email[0]
+        if email is not None:
+            session['id'] = student_code
+        otp = random.randint(100000, 999999)
+        session['otp'] = otp
+        msg_to_user = Message(f'your otp is {otp}', sender='codestech01@gmail.com', recipients=[email])
+        msg_to_user.body = "your otp is " + str(otp) 
+        mail.send(msg_to_user)
+
+        return render_template('otp_page.html')
+    
+@app.route("/verify_otp", methods=['GET', 'POST'])
+def verify_otp():
+    if request.method == 'POST':
+        otp = int(request.form['otp'])
+        generated_otp = session.get('otp')
+        if otp == generated_otp:
+            return render_template("change_pwd.html")
+        else:
+            session.pop('id')
+            return ("Wrong otp")
+
+
+@app.route("/update_pwd", methods=['GET', 'POST'])
+def update_pwd():
+    if request.method == 'POST':
+        new_pwd = request.form['new_password']
+        conf_pwd = request.form['confirm_password']
+        if new_pwd == conf_pwd:
+            student_id = session.get('id')
+            cursor.execute("UPDATE login SET Password = %s WHERE Student_ID = %s", (new_pwd, student_id))
+            mydb.commit()
+            session.pop('id')
+            session.pop('otp')
+            return redirect(url_for('login'))
+        else:
+            return "Passwords do not match."
+    return redirect(url_for('login'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
